4f3c2f5c6b5228ab5916101db678fa6888668f7b
shivani bhalani <144037713+sbhalani28@users.noreply.github.com>
2023-09-16 21:32:43 +0530

    Update lec6.2q4.c

--- lec6.2q4.c
+++ lec6.2q4.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 main()
 {
-    int f=0,s=1;
-    for
+    int f=0,s=1,n,n3;
+
+    printf("enter the number:-");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+        { printf("%d\n",f);
+         n3=f+s;
+         f=s;
+         s=n3;
+        }
 }
