47631407a73d8b0e75be7db67d632df50d9788ef
shivani bhalani <144037713+sbhalani28@users.noreply.github.com>
2023-10-18 19:33:39 +0530

    Add files via upload

---
+++ lec12.1q1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+struct Stu{
+    int id, age, std;
+    char name[40];
+    char course[10];
+    char school[50];
+    char city[15];
+
+};
+int main()
+{    int no,i;
+
+    printf("how many record add:-");
+    scanf("%d\n",&no);
+
+    for(i=0;i<no;i++){
+        struct stud s1[50];
+
+
+    }
+
+
+}
