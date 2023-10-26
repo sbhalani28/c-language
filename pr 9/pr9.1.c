c5eab7d8035335eab385aae1030f7874851753bb
shivani bhalani <144037713+sbhalani28@users.noreply.github.com>
2023-10-26 19:11:01 +0530

    Update pr9.1.c

--- pr 9/pr9.1.c
+++ pr 9/pr9.1.c
@@ -1,36 +1,20 @@
 #include<stdio.h>
-int main()
+void main()
 {
+int i;
+ FILE*fp1*fp2;
+ fp1=fopen("even.txt","w");
+ fp2=fopen("odd.txt","w");
 
- FILE*p;
- FILE*ptr;
- int i;
-
- p=fopen("even_file.txt","w");
- ptr=fopen("odd_file.txt","w");
-
- if(p && ptr == NULL)
+for(i=50;i<=70;i++)
+ 
  {
-    printf("File is unable to open");
-
+    if(i%2==0)
+     
+      {fprintf(fp1,"%d",i);}
+else{
+fprintf(fp2,"%d",i);}
  }
- else{
-    printf("file was successfully opened\n");
-
- }
- 
- for(i=50;i<=70;i++){
- 
-    if(i%2==0){
-        fprintf(p,"Even Numbers:\n%d\n",i);
-
-
-    }
-    else{
-        fprintf(ptr,"odd numbers:\n%d\n",i);
-    }
- }
- 
 }
 
 
